17d0d27dee550f9f789c7168b6bf7b08b3b5ccc6
Henry McNeil <vidmaster@gmail.com>
2016-09-18 20:04:49 -0500

    Added pom.xml file and main class

---
+++ edu/unomaha/oc/OriginalCharactersApplication.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriginalCharactersApplication {
+	public static void main(String[] args) {
+		
+	}
+}
