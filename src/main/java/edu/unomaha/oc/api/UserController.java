5d77056fb956bb6950f1b845de18b3f405fa4679
Micah <hacksmath@gmail.com>
2016-10-15 14:48:49 -0500

    Started work on a UserController
    Updated User class
    Updated HelloController to use new User class
    Created UserRowMapper class
    Created UserController class
    Added search by username, get user by id, save user, and update user
    functionalities to UserController class

---
+++ src/main/java/edu/unomaha/oc/api/UserController.java
@@ -0,0 +1,69 @@
+package edu.unomaha.oc.api;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.unomaha.oc.database.UserRowMapper;
+import edu.unomaha.oc.domain.User;
+
+@RestController
+public class UserController {
+	@Autowired
+	private DataSource dataSource;
+	
+	@RequestMapping(value="/users", method=RequestMethod.GET)
+	public ResponseEntity<List<User>> searchUsersByUsername(@RequestParam(value="username") String username) {
+		System.out.println("This is /users");
+		NamedParameterJdbcTemplate template = new NamedParameterJdbcTemplate(dataSource);
+		
+		Map<String,Object> paramMap = new HashMap<>();
+		paramMap.put("username", username);
+		
+		List<User> users = template.query("SELECT id, username, email, description, facebookId, salt, password FROM user WHERE username LIKE '%:username%'", paramMap, new UserRowMapper()); 
+		
+		return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/users/{id}", method=RequestMethod.GET)
+	public ResponseEntity<User> getUser(@PathVariable("id") int id) {
+		NamedParameterJdbcTemplate template = new NamedParameterJdbcTemplate(dataSource);
+		Map<String,Object> paramMap = new HashMap<>();
+		paramMap.put("id", id);
+		
+		User user = template.queryForObject("SELECT id, username, email, description, facebookId, salt, password FROM User WHERE id=:id", paramMap, new UserRowMapper());
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/users", method=RequestMethod.POST)
+	public void saveUser(@RequestParam(value="newuser") String newuser) {
+		NamedParameterJdbcTemplate template = new NamedParameterJdbcTemplate(dataSource);
+		Map<String,Object> paramMap = new HashMap<>();
+		paramMap.put("newuser", newuser);
+		
+		template.update("INSERT INTO User VALUES (:newuser)", paramMap);
+	}
+	
+	@RequestMapping(value="/users", method=RequestMethod.POST)
+	public void updateUser(@RequestParam(value="id") int id, @RequestParam(value="updates") String updates) {
+		NamedParameterJdbcTemplate template = new NamedParameterJdbcTemplate(dataSource);
+		Map<String,Object> paramMap = new HashMap<>();
+		paramMap.put("id", id);
+		paramMap.put("updates", updates);
+		
+		template.update("UPDATE User SET (:updates) WHERE id=:id ", paramMap);
+	}
+	
+}
--- src/main/java/edu/unomaha/oc/domain/User.java
+++ src/main/java/edu/unomaha/oc/domain/User.java
@@ -3,6 +3,11 @@ package edu.unomaha.oc.domain;
 public class User {
 	private long id;
 	private String username;
+	private String email;
+	private String description;
+	private int facebookId;
+	private long salt;
+	private String password;
 	
 	public long getId() {
 		return id;
@@ -16,17 +21,54 @@ public class User {
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getFacebookId() {
+		return facebookId;
+	}
+	public void setFacebookId(int facebookId) {
+		this.facebookId = facebookId;
+	}
+	public long getSalt() {
+		return salt;
+	}
+	public void setSalt(Long salt) {
+		this.salt = salt;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", username=" + username + "]";
 	}
-	public User(long id, String username) {
+	
+	public User(long id, String username, String email, String description, 
+				int facebookId, long salt, String password) {
 		this.id = id;
 		this.username = username;
+		this.email = email;
+		this.description = description;
+		this.facebookId = facebookId;
+		this.salt = salt;
+		this.password = password;
 	}
 	public User() {
 		
 	}
-	
 	
 }
